69e94f93fcbbf0915ea391525259297245380bce
Iaroslav Gurskyi <igu@gecko.no>
2017-07-18 17:32:05 +0300

    Add project files.

---
+++ SortingExercises/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using SortingMethods;
+
+namespace SortingExercises
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			RadixSort radixSorter = new RadixSort();
+
+			uint[] list = {1, 100,328, 3,76,499,0,14,12, 4294967295, 12, 54, 21};
+			Console.Write("Initial list: ");
+			WriteList(list);
+
+			var sortedList = radixSorter.Sort(list);
+			Console.Write("Ordered list: ");
+			WriteList(sortedList);
+
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+		}
+
+		private static void WriteList(IEnumerable<uint> list)
+		{
+			Console.WriteLine(string.Join(",", list));
+		}
+	}
+}
---
+++ SortingMethods/ISorter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SortingMethods
+{
+	public interface ISorter
+	{
+		/// <summary>
+		/// Sorts items in ascending order.
+		/// </summary>
+		/// <param name="list">Unordered collection of unsigned integers</param>
+		/// <returns>New sorted collection of unsigned integers from <param name="list"></param></returns>
+		IEnumerable<uint> Sort(IEnumerable<uint> list);
+	}
+}
